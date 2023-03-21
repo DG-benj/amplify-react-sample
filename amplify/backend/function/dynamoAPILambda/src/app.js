e5c04c464ee5ea62ac2bbdea621e15445f331b54
DG-benj <cristorson@digidelic.jp>
2023-03-21 16:04:21 +0800

    modify app.js

--- amplify/backend/function/dynamoAPILambda/src/app.js
+++ amplify/backend/function/dynamoAPILambda/src/app.js
@@ -46,20 +46,30 @@ var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware'
 var bodyParser = require('body-parser')
 var express = require('express')
 const { v4: uuidv4 } = require('uuid')
+
 AWS.config.update({ region: process.env.TABLE_REGION });
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 let tableName = "Website_PlayerData_Sample";
 if (process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
 }
-var app = express()
+var app = express()  
+app.use(bodyParser.json())  
+app.use(awsServerlessExpressMiddleware.eventContext())
+app.use(function (request, response, next) {  
+  response.header("Access-Control-Allow-Origin", "\*")  
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")  
+  next()  
+});
+
+/*var app = express()
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*")
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
-});
+});*/
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
@@ -97,6 +107,8 @@ app.get("/dynamoAPIs", function (request, response) {
     }
   });
 });
+
+
 
 
 app.get(path + hashKeyPath, function(req, res) {
